7f1a8d05c653591f8d66d9730298773ccb108574
cynthia-75 <cynthiaokereke75@gmail.com>
2023-06-20 07:40:52 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,14 +1,28 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - Entry point
  *
  * Return: Always 0
  */
+
 int main(void)
 
 {
-	print_alphabet_x10();
-	return (0);
+	char ch;
+	int i;
 
+	i = 0;
+
+	while (i < 10)
+	{
+		ch = 'a';
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			ch++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }
