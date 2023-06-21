9cfab9848e4d72d9a37ae5966ee92f11d450824c
cynthia-75 <cynthiaokereke75@gmail.com>
2023-06-21 15:15:30 -0700

    100-times_table.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+* fib - Prints fibonacci numbers of 50
+*/
+void fib(void)
+{
+	unsigned long i;
+	unsigned long f = 0;
+	unsigned long s = 1;
+	unsigned long r;
+	
+	for (i = 1; i <= 50; i++)
+        {
+		r = f + s;
+                printf("%lu, ", r);
+                f = s;
+                s = r;
+        }
+}
