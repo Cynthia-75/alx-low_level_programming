8a87b73981f955c37101bc756113e80e00561bee
cynthia-75 <cynthiaokereke75@gmail.com>
2023-06-26 07:32:36 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,12 +6,12 @@
  */
 void print_rev(char *s)
 {
-	int 1 = 0;
+	int i = 0;
 
 	while (s[i] != '\0')
 		i++;
 	for (i = i - 1; i >= 0; i--)
-		_putchar(s[i];
+		_putchar(s[i]);
 
 	_putchar('\n');
 }
