92cf1279e49650da79a67f3e58625831eb96df3c
cynthia-75 <cynthiaokereke75@gmail.com>
2023-06-29 12:30:23 -0700

    2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -3,11 +3,11 @@
 /**
  * _strncpy - copys a string with n
  * @dest: copy to
- * @stc: copy from
+ * @src: copy from
  * @n: number of char to be copied
  * Return: dest
  */
-char *_strncpy(char *dest, char *stc, int n)
+char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
